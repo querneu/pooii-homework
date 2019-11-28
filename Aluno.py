c4d0fbe2e3bf38d0b097dd3c39cc620efd2dade9
Lucas <lucasoaresleite@gmail.com>
2019-11-28 14:09:36 -0300

    done Aluno

--- Aluno.py
+++ Aluno.py
@@ -1,11 +1,7 @@
-from .Pessoa import  Pessoa
-
-class Aluno:
-    def __init__(self,Pessoa, matricula):
-        self._nome = Pessoa._nome
-        self._sobrenome = Pessoa._sobrenome
-        self._matricula = matricula
-    
+from Pessoa import Pessoa
+class Aluno(Pessoa):        
+    def __init__(self):
+        pass
     def getMatricula(self):
         return self._matricula
     
