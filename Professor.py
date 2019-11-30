99cd84d98819a063dc14eb0d2cbf8a9633f65b2b
Richarde <lucasoaresleite@gmail.com>
2019-11-30 16:39:57 -0300

    add inline comments to Professor class

--- Professor.py
+++ Professor.py
@@ -1,9 +1,14 @@
+# Importe da classe pai para aluno receber atributos de herança
 from Pessoa import Pessoa
+# Criação de classe aluno já instanciando pessoa
 class Professor(Pessoa):
     def __init__(self):
         pass
+    
+    # Metodo getMatricula para retornar o valor da matricula solicitada
     def getCodigo(self):
         return self._codigo
     
+    # Metodo serMatricula para inserir o valor da matricula
     def setCodigo(self, codigo):
         self._codigo = codigo
