cd8b1862ec0e42e93050830b3676c2acf6056293
Richarde <=>
2019-11-30 16:54:56 -0300

    fix Aluno class comments

--- Aluno.py
+++ Aluno.py
@@ -1,15 +1,15 @@
-# Importe da classe pai para aluno receber atributos de herança
+# Importe da classe pai para aluno receber atributos de herança.
 from Pessoa import Pessoa
 
-# Criação de classe aluno já instanciando pessoa
+# Criação de classe aluno já instanciando pessoa.
 class Aluno(Pessoa):        
     def __init__(self):
         pass
 
-    # Metodo getMatricula para retornar o valor da matricula solicitada
+    # Metodo getMatricula para retornar o valor da matricula solicitada.
     def getMatricula(self):
         return self._matricula
     
-    # Metodo serMatricula para inserir o valor da matricula
+    # Metodo serMatricula para inserir o valor da matricula.
     def setMatricula(self,matricula):
         self._matricula = matricula
